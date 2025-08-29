40180935b92e34e1796bdc78db599c086ae14511
Connor Welsh <cwel@cwel.sh>
2025-08-28 20:51:51 -0400

    Refactor terminal title handling in zsh plugin
    Refactor title function and improve terminal title handling

--- zsh-tab-title.plugin.zsh
+++ zsh-tab-title.plugin.zsh
@@ -1,170 +1,164 @@
-#!/bin/bash
-
 # Set terminal window and tab/icon title
 #
-# usage: title short_tab_title long_window_title
+# usage: title short_tab_title [long_window_title]
 #
 # See: http://www.faqs.org/docs/Linux-mini/Xterm-Title.html#ss3.1
-# Fully supports screen, hyper, iterm, wezterm, and probably most modern xterm and rxvt
+# Fully supports screen, iterm, and probably most modern xterm and rxvt
 # (In screen, only short_tab_title is used)
-#
-# Enhanced WezTerm support with OSC 1337 user vars and OSC 7 working directory
-# For SSH sessions, ensure this plugin is installed on remote hosts for best experience
-#
-# Debug: Set ZSH_TAB_TITLE_DEBUG=true to see what's happening
-# Example: export ZSH_TAB_TITLE_DEBUG=true
+# Limited support for Apple Terminal (Terminal can't set window and tab separately)
 function title {
-  emulate -L zsh
-  setopt prompt_subst
-  
-  [[ "$EMACS" == *term* ]] && return
+  setopt localoptions nopromptsubst
 
-  tabTitle="$1"
-  termTitle="$2"
+  # Don't set the title if inside emacs, unless using vterm
+  [[ -n "${INSIDE_EMACS:-}" && "$INSIDE_EMACS" != vterm ]] && return
 
-  if [[ "$ZSH_TAB_TITLE_DEBUG" == "true" ]]; then
-    echo "[DEBUG] title() called - tabTitle='$tabTitle', termTitle='$termTitle'"
-    echo "[DEBUG] TERM_PROGRAM='$TERM_PROGRAM', TERM='$TERM'"
-  fi
+  # if $2 is unset use $1 as default
+  # if it is set and empty, leave it as is
+  : ${2=$1}
 
-  if [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
-    print -Pn "\e]2;$termTitle:q\a" # set window name
-    print -Pn "\e]1;$tabTitle:q\a" # set tab name
-  elif [[ "$TERM_PROGRAM" == "Hyper" ]]; then
-    print -Pn "\e]1;$termTitle:q\a" # set tab name
-    print -Pn "\e]2;$tabTitle:q\a" # set window name
-  elif [[ "$TERM_PROGRAM" == "WezTerm" ]]; then
-    print -Pn "\e]2;$termTitle:q\a" # set window name
-    print -Pn "\e]1;$tabTitle:q\a" # set tab name
-  else
-    if [[ "$ZSH_TAB_TITLE_DEBUG" == "true" ]]; then
-      echo "[DEBUG] Using fallback title setting for TERM='$TERM'"
-    fi
-    case "$TERM" in
-      xterm-kitty)
-        print -Pn "\e]1;$termTitle:q\a" # set window name
-        print -Pn "\e]2;$tabTitle:q\a" # set tab name
+  case "$TERM" in
+    cygwin|xterm*|putty*|rxvt*|konsole*|ansi|mlterm*|alacritty*|st*|foot*|contour*|wezterm*)
+      print -Pn "\e]2;${2:q}\a" # set window name
+      print -Pn "\e]1;${1:q}\a" # set tab name
       ;;
-
-      wezterm)
-        print -Pn "\e]2;$termTitle:q\a" # set window name
-        print -Pn "\e]1;$tabTitle:q\a" # set tab name
+    screen*|tmux*)
+      print -Pn "\ek${1:q}\e\\" # set screen hardstatus
       ;;
-
-      cygwin|xterm*|putty*|rxvt*|ansi|${~ZSH_TAB_TITLE_ADDITIONAL_TERMS})
-        print -Pn "\e]2;$termTitle:q\a" # set window name
-        print -Pn "\e]1;$tabTitle:q\a" # set tab name
+    *)
+      if [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
+        print -Pn "\e]2;${2:q}\a" # set window name
+        print -Pn "\e]1;${1:q}\a" # set tab name
+      else
+        # Try to use terminfo to set the title if the feature is available
+        if (( ${+terminfo[fsl]} && ${+terminfo[tsl]} )); then
+          print -Pn "${terminfo[tsl]}$1${terminfo[fsl]}"
+        fi
+      fi
       ;;
-
-      screen*|tmux*)
-        print -Pn "\ek$tabTitle:q\e\\" # set screen hardstatus
-      ;;
-    esac
-  fi
+  esac
 }
 
-function setTerminalTitleInIdle {
-
-  if [[ "$ZSH_TAB_TITLE_DISABLE_AUTO_TITLE" == true ]]; then
-    return
-  fi
-
-  if [[ "$ZSH_TAB_TITLE_ONLY_FOLDER" == true ]]; then
-    ZSH_THEME_TERM_TAB_TITLE_IDLE=${PWD##*/}
-  else
-    ZSH_THEME_TERM_TAB_TITLE_IDLE="%20<..<%~%<<" #15 char left truncated PWD
-  fi
-
-  if [[ "$ZSH_TAB_TITLE_DEFAULT_DISABLE_PREFIX" == true ]]; then
-  ZSH_TAB_TITLE_PREFIX=""
-  elif [[ -z "$ZSH_TAB_TITLE_PREFIX" ]]; then
-    ZSH_TAB_TITLE_PREFIX="%n@%m:"
-  fi
-
-  ZSH_THEME_TERM_TITLE_IDLE="$ZSH_TAB_TITLE_PREFIX %~ $ZSH_TAB_TITLE_SUFFIX"
-
-  title "$ZSH_THEME_TERM_TAB_TITLE_IDLE" "$ZSH_THEME_TERM_TITLE_IDLE"
-  
-  # WezTerm-specific: Set OSC 1337 user vars for better integration
-  # Check multiple ways to detect WezTerm, or assume WezTerm in SSH sessions
-  local is_wezterm=false
-  
-  if [[ "$TERM_PROGRAM" == "WezTerm" ]] || [[ "$WEZTERM_PANE" != "" ]] || [[ "$TERM_PROGRAM_VERSION" =~ "wezterm" ]]; then
-    is_wezterm=true
-  elif [[ "$SSH_CONNECTION" != "" ]] && [[ "$ZSH_TAB_TITLE_ASSUME_WEZTERM_SSH" == "true" ]]; then
-    # Assume WezTerm in SSH sessions if explicitly enabled
-    is_wezterm=true
-  fi
-  
-  if [[ "$is_wezterm" == "true" ]]; then
-    if [[ "$ZSH_TAB_TITLE_DEBUG" == "true" ]]; then
-      echo "[DEBUG] WezTerm detected/assumed - setting OSC sequences"
-      echo "[DEBUG] TERM_PROGRAM=$TERM_PROGRAM, WEZTERM_PANE=$WEZTERM_PANE, SSH_CONNECTION=$SSH_CONNECTION"
-      echo "[DEBUG] HOST=$HOST, PWD=$PWD"
-    fi
-    printf "\033]1337;SetUserVar=WEZTERM_PROG=%s\033\\" "$(echo -n "zsh" | base64)"
-    printf "\033]1337;SetUserVar=WEZTERM_USER=%s\033\\" "$(echo -n "$USER" | base64)"
-    printf "\033]1337;SetUserVar=WEZTERM_HOST=%s\033\\" "$(echo -n "$HOST" | base64)"
-    # Set current working directory with OSC 7
-    printf "\033]7;file://%s%s\033\\" "$HOST" "$PWD"
-  elif [[ "$ZSH_TAB_TITLE_DEBUG" == "true" ]]; then
-    echo "[DEBUG] WezTerm not detected - TERM_PROGRAM=$TERM_PROGRAM, WEZTERM_PANE=$WEZTERM_PANE, SSH_CONNECTION=$SSH_CONNECTION"
-  fi
-}
+ZSH_THEME_TERM_TAB_TITLE_IDLE="%15<..<%~%<<" #15 char left truncated PWD
+ZSH_THEME_TERM_TITLE_IDLE="%~"
+# Avoid duplication of directory in terminals with independent dir display
+if [[ "$TERM_PROGRAM" == Apple_Terminal ]]; then
+  ZSH_THEME_TERM_TITLE_IDLE="%~"
+fi
 
 # Runs before showing the prompt
 function omz_termsupport_precmd {
-  emulate -L zsh
-
-  setTerminalTitleInIdle
+  [[ "${DISABLE_AUTO_TITLE:-}" != true ]] || return 0
+  title "$ZSH_THEME_TERM_TAB_TITLE_IDLE" "$ZSH_THEME_TERM_TITLE_IDLE"
 }
 
 # Runs before executing the command
 function omz_termsupport_preexec {
+  [[ "${DISABLE_AUTO_TITLE:-}" != true ]] || return 0
+
   emulate -L zsh
   setopt extended_glob
 
-  if [[ "$ZSH_TAB_TITLE_DISABLE_AUTO_TITLE" == true ]]; then
-    return
+  # split command into array of arguments
+  local -a cmdargs
+  cmdargs=("${(z)2}")
+  # if running fg, extract the command from the job description
+  if [[ "${cmdargs[1]}" = fg ]]; then
+    # get the job id from the first argument passed to the fg command
+    local job_id jobspec="${cmdargs[2]#%}"
+    # logic based on jobs arguments:
+    # http://zsh.sourceforge.net/Doc/Release/Jobs-_0026-Signals.html#Jobs
+    # https://www.zsh.org/mla/users/2007/msg00704.html
+    case "$jobspec" in
+      <->) # %number argument:
+        # use the same <number> passed as an argument
+        job_id=${jobspec} ;;
+      ""|%|+) # empty, %% or %+ argument:
+        # use the current job, which appears with a + in $jobstates:
+        # suspended:+:5071=suspended (tty output)
+        job_id=${(k)jobstates[(r)*:+:*]} ;;
+      -) # %- argument:
+        # use the previous job, which appears with a - in $jobstates:
+        # suspended:-:6493=suspended (signal)
+        job_id=${(k)jobstates[(r)*:-:*]} ;;
+      [?]*) # %?string argument:
+        # use $jobtexts to match for a job whose command *contains* <string>
+        job_id=${(k)jobtexts[(r)*${(Q)jobspec}*]} ;;
+      *) # %string argument:
+        # use $jobtexts to match for a job whose command *starts with* <string>
+        job_id=${(k)jobtexts[(r)${(Q)jobspec}*]} ;;
+    esac
+
+    # override preexec function arguments with job command
+    if [[ -n "${jobtexts[$job_id]}" ]]; then
+      1="${jobtexts[$job_id]}"
+      2="${jobtexts[$job_id]}"
+    fi
   fi
 
-  if [[ "$ZSH_TAB_TITLE_ENABLE_FULL_COMMAND" == true ]]; then
-  	  # full command
-	  local CMD=${1:gs/%/%%}
-    local LINE=${2:gs/%/%%}
-  else
-	  # cmd name only, or if this is sudo or ssh, the next cmd
-	  local CMD=${1[(wr)^(*=*|sudo|ssh|mosh|rake|-*)]:gs/%/%%}
-    local LINE=${2[(wr)^(*=*|sudo|ssh|mosh|rake|-*)]:gs/%/%%}
-  fi
+  # cmd name only, or if this is sudo or ssh, the next cmd
+  local CMD="${1[(wr)^(*=*|sudo|ssh|mosh|rake|-*)]:gs/%/%%}"
+  local LINE="${2:gs/%/%%}"
 
-  if [[ "$ZSH_TAB_TITLE_CONCAT_FOLDER_PROCESS" == true ]]; then
-    title "${PWD##*/}:%100>...>$LINE%<<" "${PWD##*/}:${CMD}"
-  else
-    title "%100>...>$LINE%<<" "$CMD"
-  fi
-  
-  # WezTerm-specific: Update user vars when executing commands
-  # Check multiple ways to detect WezTerm, or assume WezTerm in SSH sessions
-  local is_wezterm=false
-  
-  if [[ "$TERM_PROGRAM" == "WezTerm" ]] || [[ "$WEZTERM_PANE" != "" ]] || [[ "$TERM_PROGRAM_VERSION" =~ "wezterm" ]]; then
-    is_wezterm=true
-  elif [[ "$SSH_CONNECTION" != "" ]] && [[ "$ZSH_TAB_TITLE_ASSUME_WEZTERM_SSH" == "true" ]]; then
-    # Assume WezTerm in SSH sessions if explicitly enabled
-    is_wezterm=true
-  fi
-  
-  if [[ "$is_wezterm" == "true" ]]; then
-    printf "\033]1337;SetUserVar=WEZTERM_PROG=%s\033\\" "$(echo -n "$CMD" | base64)"
-    # Update current working directory with OSC 7
-    printf "\033]7;file://%s%s\033\\" "$HOST" "$PWD"
-  fi
+  title "$CMD" "%100>...>${LINE}%<<"
 }
 
-# Execute the first time, so it show correctly on terminal load
-setTerminalTitleInIdle
+autoload -Uz add-zsh-hook
 
-autoload -U add-zsh-hook
-add-zsh-hook precmd omz_termsupport_precmd
-add-zsh-hook preexec omz_termsupport_preexec
+if [[ -z "$INSIDE_EMACS" || "$INSIDE_EMACS" = vterm ]]; then
+  add-zsh-hook precmd omz_termsupport_precmd
+  add-zsh-hook preexec omz_termsupport_preexec
+fi
+
+# Keep terminal emulator's current working directory correct,
+# even if the current working directory path contains symbolic links
+#
+# References:
+# - Apple's Terminal.app: https://superuser.com/a/315029
+# - iTerm2: https://iterm2.com/documentation-escape-codes.html (iTerm2 Extension / CurrentDir+RemoteHost)
+# - Konsole: https://bugs.kde.org/show_bug.cgi?id=327720#c1
+# - libvte (gnome-terminal, mate-terminal, …): https://bugzilla.gnome.org/show_bug.cgi?id=675987#c14
+#   Apparently it had a bug before ~2012 were it would display the unknown OSC 7 code
+#
+# As of May 2021 mlterm, PuTTY, rxvt, screen, termux & xterm simply ignore the unknown OSC.
+
+# Don't define the function if we're inside Emacs or in an SSH session (#11696)
+if [[ -n "$INSIDE_EMACS" || -n "$SSH_CLIENT" || -n "$SSH_TTY" ]]; then
+  return
+fi
+
+# Don't define the function if we're in an unsupported terminal
+case "$TERM" in
+  # all of these either process OSC 7 correctly or ignore entirely
+  xterm*|putty*|rxvt*|konsole*|mlterm*|alacritty*|screen*|tmux*) ;;
+  contour*|foot*) ;;
+  *)
+    # Terminal.app and iTerm2 process OSC 7 correctly
+    case "$TERM_PROGRAM" in
+      Apple_Terminal|iTerm.app) ;;
+      *) return ;;
+    esac ;;
+esac
+
+# Emits the control sequence to notify many terminal emulators
+# of the cwd
+#
+# Identifies the directory using a file: URI scheme, including
+# the host name to disambiguate local vs. remote paths.
+function omz_termsupport_cwd {
+  setopt localoptions unset
+  # Percent-encode the host and path names.
+  local URL_HOST URL_PATH
+  URL_HOST="$(omz_urlencode -P $HOST)" || return 1
+  URL_PATH="$(omz_urlencode -P $PWD)" || return 1
+
+  # Konsole errors if the HOST is provided
+  [[ -z "$KONSOLE_PROFILE_NAME" && -z "$KONSOLE_DBUS_SESSION"  ]] || URL_HOST=""
+
+  # common control sequence (OSC 7) to set current host and path
+  printf "\e]7;file://%s%s\e\\" "${URL_HOST}" "${URL_PATH}"
+}
+
+# Use a precmd hook instead of a chpwd hook to avoid contaminating output
+# i.e. when a script or function changes directory without `cd -q`, chpwd
+# will be called the output may be swallowed by the script or function.
+add-zsh-hook precmd omz_termsupport_cwd
